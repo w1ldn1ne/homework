5d26dd419e40f5434fefc36716a2b11d8405726c
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-18 18:57:23 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var weightFirstFighter = 78.2;
         var weightSecondFighter = 82.7;
-        System.out.println(weightFirstFighter + weightSecondFighter);
+        var fullWeight = weightFirstFighter + weightSecondFighter;
+        var weightMinus = weightSecondFighter - weightFirstFighter;
+        System.out.println("Общий вес = " + fullWeight);
+        System.out.println("Разница в весе = " + weightMinus);
     }
 }
