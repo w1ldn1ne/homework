5cb2d1ad60d36da45d675ae4c18fef0a3b1ecae1
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-18 18:49:53 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        System.out.println(friend);
-        System.out.println(friend + 2);
-        System.out.println((friend +2) / 7);
+        var frog = 3.5;
+        System.out.println(frog);
+        System.out.println(frog * 10);
+        System.out.println((frog * 10) / 3.5);
+        System.out.println(((frog * 10) / 3.5) + 4);
     }
 }
