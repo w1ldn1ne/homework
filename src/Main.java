fb16c9f2a26c1a4775284f9544082ba01c4fa217
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-18 18:26:03 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        var number = 4;
         var dog = 8.0;
-        System.out.println(dog);
+        System.out.println(dog + number);
         var cat = 3.6;
-        System.out.println(cat);
+        System.out.println(cat + number);
         var paper = 763789;
-        System.out.println(paper);
+        System.out.println(paper + number);
     }
 }
