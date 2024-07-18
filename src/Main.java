e1e1725ee684315e90e6bd5fefc80586e168b1c2
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-18 18:47:11 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var numberAllPlus = 4;
-        var dog = 8.0;
-        var numberDogMinus = 3.5;
-        System.out.println((dog + numberAllPlus) - numberDogMinus);
-        var cat = 3.6;
-        var numberCatMinus = 1.6;
-        System.out.println((cat + numberAllPlus) - numberCatMinus);
-        var paper = 763789;
-        var numberPaperMinus = 7639;
-        System.out.println((paper + numberAllPlus) - numberPaperMinus);
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend + 2);
+        System.out.println((friend +2) / 7);
     }
 }
