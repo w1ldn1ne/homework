ca5eedfe776108e4d6c85aa298667ba8ac5b518d
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:16:08 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -84,3 +84,14 @@ public class Main {
 }
 
 //Восьмое задание
+public class Main {
+    public static void main(String[] args) {
+        var fullTime = 640;
+        var timeOne = 8;
+        var people = fullTime / timeOne;
+        System.out.println("Всего работников в компании - " + people + " человек.");
+        var peoplePlus = people + 94;
+        var timeOnePlus = fullTime / peoplePlus;
+        System.out.println("Если в компании работает " + peoplePlus + " человека, то всего " + timeOnePlus + " часа работы может быть поделено между сотрудниками.");
+    }
+}
