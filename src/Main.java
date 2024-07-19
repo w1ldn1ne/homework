0e38757d4b8500546c694831292c885972d1e063
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:12:53 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -51,3 +51,14 @@ public class Main {
 }
 
 //Пятое задание
+public class Main {
+    public static void main(String[] args) {
+        var frog = 3.5;
+        System.out.println(frog);
+        System.out.println(frog * 10);
+        System.out.println((frog * 10) / 3.5);
+        System.out.println(((frog * 10) / 3.5) + 4);
+    }
+}
+
+//Шестое задание
