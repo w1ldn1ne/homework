e89f19dd043d9a79fa061be46ceed774b17a708c
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:11:22 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -25,3 +25,19 @@ public class Main {
 }
 
 //Третье задание
+public class Main {
+    public static void main(String[] args) {
+        var numberAllPlus = 4;
+        var dog = 8.0;
+        var numberDogMinus = 3.5;
+        System.out.println((dog + numberAllPlus) - numberDogMinus);
+        var cat = 3.6;
+        var numberCatMinus = 1.6;
+        System.out.println((cat + numberAllPlus) - numberCatMinus);
+        var paper = 763789;
+        var numberPaperMinus = 7639;
+        System.out.println((paper + numberAllPlus) - numberPaperMinus);
+    }
+}
+
+//Четвертое задание
