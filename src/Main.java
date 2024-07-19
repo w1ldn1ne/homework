41122ca3e188b47667f44edb53e6bcf17f7f7c03
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:12:15 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -41,3 +41,13 @@ public class Main {
 }
 
 //Четвертое задание
+public class Main {
+    public static void main(String[] args) {
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend + 2);
+        System.out.println((friend +2) / 7);
+    }
+}
+
+//Пятое задание
