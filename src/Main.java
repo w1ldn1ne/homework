125070810746d911e8e98c0366dc26aa0016bbcb
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:13:23 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -62,3 +62,15 @@ public class Main {
 }
 
 //Шестое задание
+public class Main {
+    public static void main(String[] args) {
+        var weightFirstFighter = 78.2;
+        var weightSecondFighter = 82.7;
+        var fullWeight = weightFirstFighter + weightSecondFighter;
+        var weightMinus = weightSecondFighter - weightFirstFighter;
+        System.out.println("Общий вес = " + fullWeight);
+        System.out.println("Разница в весе = " + weightMinus);
+    }
+}
+
+//Седьмое задание
