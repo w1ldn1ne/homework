0484ffe021efeb1fba61fef6198873907273e48a
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:10:44 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+//Первое задание
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+    }
+}
+
+
+//Второе задание
 public class Main {
     public static void main(String[] args) {
         var number = 4;
@@ -9,3 +23,5 @@ public class Main {
         System.out.println(paper + number);
     }
 }
+
+//Третье задание
