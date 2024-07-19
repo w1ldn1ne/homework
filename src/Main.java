f4cadeb6847e0cadc3c77122b687f7f1903847d8
w1ldn1ne <w1ldn1ne@icloud.com>
2024-07-19 14:13:58 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -74,3 +74,13 @@ public class Main {
 }
 
 //Седьмое задание
+public class Main {
+    public static void main(String[] args) {
+        var weightFirstFighter = 78.2;
+        var weightSecondFighter = 82.7;
+        var weightOver = weightSecondFighter % weightFirstFighter;
+        System.out.println("Остаток = " + weightOver);
+    }
+}
+
+//Восьмое задание
